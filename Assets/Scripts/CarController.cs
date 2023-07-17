98214694e958e6333894d28702a0c7a9e017a628
Jonathan <irwinjonathan3@gmail.com>
2023-07-17 23:41:51 +0100

    Added Colliders and switched to velocity based movement
    Car does not function as it did before and colliders need lots of work to function well

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     private float move, moveSpeed, rotation, rotationSpeed;
+    public Rigidbody2D rb;
+    private Vector2 movedir;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +18,22 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = Input.GetAxis("Vertical")* moveSpeed * Time.deltaTime;
-        rotation = Input.GetAxis("Horizontal")* -rotationSpeed * Time.deltaTime;
+        //move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //rotation = Input.GetAxis("Horizontal") * -rotationSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+        movedir = new Vector2(moveX,moveY).normalized;
     }
 
     private void LateUpdate()
     {
-            transform.Translate(0f,move,0f);
-            transform.Rotate(0f,0f,rotation);
+            //transform.Translate(0f,move,0f);
+            //transform.Rotate(0f,0f,rotation);
 
+    }
+
+    private void FixedUpdate()
+    {
+            rb.velocity = new Vector2(movedir.x * moveSpeed , movedir.y * moveSpeed);
     }
 }
