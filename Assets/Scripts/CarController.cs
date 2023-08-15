75cb041418e51cf325866a36a40bfdffd851db76
FileFoundex <irwinjonathan3@gmail.com>
2023-08-15 01:05:31 +0100

    Added Car and car controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,39 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private float move, moveSpeed, rotation, rotationSpeed;
-    public Rigidbody2D rb;
-    private Vector2 movedir;
+    [Header("Car Settings")]
+    public float accelerationFactor = 30.0f;
+    public float turnFactor = 3.5f;
+
+    //local variables
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+
+    //Components 
+    Rigidbody2D carRigidbody2D;
+
+
+    //awake is called when the script is being loaded
+    private void Awake()
+    {
+        carRigidbody2D = GetComponent<Rigidbody2D>();   
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 5f;
-        rotationSpeed = 200f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        //rotation = Input.GetAxis("Horizontal") * -rotationSpeed * Time.deltaTime;
-        float moveY = Input.GetAxis("Vertical");
-        float moveX = Input.GetAxis("Horizontal");
-        movedir = new Vector2(moveX,moveY).normalized;
-    }
-
-    private void LateUpdate()
-    {
-            //transform.Translate(0f,move,0f);
-            //transform.Rotate(0f,0f,rotation);
 
     }
 
     private void FixedUpdate()
     {
-            rb.velocity = new Vector2(movedir.x * moveSpeed , movedir.y * moveSpeed);
+        ApplyEngineForce();
+
+        ApplySteering();
+        
     }
+
+    void ApplyEngineForce()
+    {
+        //create a force for the engine
+        Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
+
+        //apply force and pushes car forward
+        carRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
+
+    }
+
+    void ApplySteering()
+    {
+        //Update the rotation angle based on input
+        rotationAngle -= steeringInput * turnFactor;
+
+        //Appply steering by rotating the car object 
+        carRigidbody2D.MoveRotation(rotationAngle);
+
+        
+    }
+
+    public void setInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+
+
+
+
 }
